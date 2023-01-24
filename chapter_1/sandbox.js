b066fcb735f410e0211a820ec8b0a4166ff66853
MichaelAbe <michael.k.abe@gmail.com>
2023-01-24 10:36:17 -0500

    variables

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,1 +1,15 @@
-alert("Other page");
+let age = 36; 
+let year = 2023;
+
+
+
+console.log(age, year);
+
+age = 50;
+console.log(age);
+
+const points = (100);
+console.log(points);
+
+var score = 75; 
+console.log(score);

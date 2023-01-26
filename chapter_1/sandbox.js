30c6abc9e8cf0e1619d4a7e1b03c7846a22c2977
MichaelAbe <michael.k.abe@gmail.com>
2023-01-26 12:40:18 -0500

    arrow functions

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -1,18 +1,27 @@
-// const names = ['Kek', 'Jiggz', 'Tonka', 'Yeti'];
-// const age = 25;
-
-// if(age > 20){
-//     console.log('You are over 20 years old');
-// }
-
-// if(names.length > 3){
-//     console.log("That's a straight up squad");
-// }
-
-const password = "passwo";
-
-if(password.length >= 8){
-    console.log("That password is long enough")
-} else {
-    console.log("That passowrd is weak AFs")
+function greet(){
+    console.log("Hello there")
 }
+
+greet()
+
+let greeting = (name) => console.log("Hi, my name is " + name)
+
+let pets = ["Tonka", "Yeti", "The Puppy", "Jello", "Beau"]
+
+let greetings = (arr) => {
+    for(let i = 0; i < arr.length; i++){
+        console.log(`Hello, I am a pet and my name is ${arr[i]}`)
+    }
+}
+
+greeting("Kek")
+greeting("Tonka")
+greetings(pets)
+
+const calcArea = (radius) => {
+    return area = Math.PI * radius**2
+} 
+
+const area = calcArea(3)
+
+console.log(area)
